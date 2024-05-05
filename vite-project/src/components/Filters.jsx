290365f6500407befa42d8343b7ffcdb6be69ab3
trinidad <daianatrinidadpuig@gmail.com>
2024-05-05 15:54:56 -0300

    Se creó la data JSON del proyecto y se realizaron los filtros ppara buscar por precio y por categoria

---
+++ vite-project/src/components/Filters.jsx
@@ -0,0 +1,44 @@
+import { useState, useId } from "react";
+import "./Filters.css";
+export function Filters({onChange}) {
+  const [minPrice, setMinPrice] = useState(0);
+  const minPriceFilterId=useId()
+ const minCategoryFilterId=useId() 
+  const handleChangeMinPrice = (event) => {
+    setMinPrice(event.target.value)
+    onChange(prevState=>({
+    ...prevState,
+    minPrice:event.target.value
+    }))
+  }
+  const handleChangeCategory=(event)=>{
+    onChange(prevState=>({
+        ...prevState,
+        category:event.target.value
+    }))
+  }
+  return (
+    <section className="filters">
+      <div>
+        <label htmlFor={minPriceFilterId}>Precio a partir de :</label>
+        <input
+          type="range"
+          id={minPriceFilterId}
+          min="0"
+          max="1000"
+          onChange={handleChangeMinPrice}
+        />
+        <span>${minPrice}</span>
+      </div>
+
+      <div>
+        <label htmlFor={minCategoryFilterId}>Categoria</label>
+        <select id={minCategoryFilterId} onChange={handleChangeCategory}>
+          <option value="all">Todas</option>
+          <option value="laptops">Portátiles</option>
+          <option value="smartphones">Celulares</option>
+        </select>
+      </div>
+    </section>
+  );
+}
